42db56938bc742d389570ee38d75eab8690fea63
clucier <onedudedesign@gmail.com>
2017-04-04 15:46:16 -0700

    added a recycleview.viewholder to hod the references to the imagviews for the recyclerview to create the grid.

--- app/src/main/java/com/onedudedesign/popularmoviess1/MainActivity.java
+++ app/src/main/java/com/onedudedesign/popularmoviess1/MainActivity.java
@@ -3,6 +3,8 @@ package com.onedudedesign.popularmoviess1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+
+    //The MovieViewHolder Class extending recyyclerview viewholder to hold
+    // references to the image views in the layout
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+
+        //the constructor
+        public MovieViewHolder (View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.moviePosterImageView);
+        }
     }
 }
