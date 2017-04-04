b0f25c814b78f85bc9b4d5d80a9e5610546791d7
clucier <onedudedesign@gmail.com>
2017-04-04 15:22:41 -0700

    Created the Movie class to create the object for Each Movie

---
+++ app/src/main/java/com/onedudedesign/popularmoviess1/Movie.java
@@ -0,0 +1,66 @@
+package com.onedudedesign.popularmoviess1;
+
+/**
+ * Created by clucier on 4/4/17.
+ * This is the class to define a Movie Object to be used by the Adapter
+ */
+
+public class Movie {
+    //Setup the data variables
+    private String movieTitle;
+    private String moviePoster;
+    private String movieSynopsis;
+    private String movieTmdbRating;
+    private String movieReleaseDate;
+
+    //method to get the movie title
+    public String getMovieTitle () {
+        return movieTitle;
+    }
+
+    //method to set the title of the movie
+    public void setMovieTitle (String title) {
+        this.movieTitle = title;
+    }
+
+    //method to get the string location of the movie poster
+    public String getMoviePoster () {
+        return moviePoster;
+    }
+
+    //method to set the poster string
+    public void setMoviePoster (String poster) {
+        this.moviePoster = poster;
+    }
+
+    //method to get the movie synopsis
+    public String getMovieSynopsis () {
+        return movieSynopsis;
+    }
+
+    //method to set the movie synopsis
+    public void setMovieSynopsis (String synopsis) {
+        this.movieSynopsis = synopsis;
+    }
+
+    //method to set the rating from TMDB.org
+    public String getMovieTmdbRating () {
+        return movieTmdbRating;
+    }
+
+    //method to get the TMDB.org rating
+    public void setMovieTmdbRating (String rating) {
+        this.movieTmdbRating = rating;
+    }
+
+    //method to get the release date (in string ay need to look at format???
+    public String getMovieReleaseDate () {
+        return movieReleaseDate;
+    }
+
+    //method to set the release date from the TMDB.org DB
+    public void setMovieReleaseDate (String releaseDate) {
+        this.movieReleaseDate = releaseDate;
+    }
+
+}
