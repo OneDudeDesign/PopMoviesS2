805f0c365c59e25765ab03803aaf2e1a5e27552f
clucier <onedudedesign@gmail.com>
2017-04-07 15:49:42 -0700

    Pulling data based on a fixed ID need the intent to send the string for the ID and then send the id to the APIService

--- app/src/main/java/com/onedudedesign/popularmoviess1/DetailActivity.java
+++ app/src/main/java/com/onedudedesign/popularmoviess1/DetailActivity.java
@@ -2,6 +2,7 @@ package com.onedudedesign.popularmoviess1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.onedudedesign.popularmoviess1.Models.MovieDetail;
 import com.onedudedesign.popularmoviess1.utils.MovieApiService;
@@ -15,6 +16,7 @@ import retrofit.client.Response;
 public class DetailActivity extends AppCompatActivity {
 
     private static final String TMDB_API_KEY = "fd66dfefac539c5f745200aadb175e4d";
+    MovieDetail mDetail = new MovieDetail();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +39,8 @@ public class DetailActivity extends AppCompatActivity {
         service.getMovie(new Callback<MovieDetail>() {
             @Override
             public void success(MovieDetail movieDetail, Response response) {
-
+                mDetail = movieDetail;
+                populateData();
             }
 
             @Override
@@ -45,5 +48,28 @@ public class DetailActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
+    }
+
+    private void populateData () {
+        TextView title = (TextView) findViewById(R.id.movieTitle);
+        title.setText("Movie Title : " + mDetail.getMovieTitle());
+
+        TextView id = (TextView) findViewById(R.id.movieID);
+        id.setText("Movie ID : " + mDetail.getMovieID());
+
+        TextView poster = (TextView) findViewById(R.id.moviePosterPath);
+        poster.setText("Movie Poster Path : " + mDetail.getMoviePoster());
+
+        TextView overview = (TextView) findViewById(R.id.movieOverview);
+        overview.setText("Movie Overview : " + mDetail.getMovieSynopsis());
+
+        TextView vote = (TextView) findViewById(R.id.movieVoteAverage);
+        vote.setText("Movie Vote Average : " + mDetail.getMovieTmdbRating());
+
+        TextView release = (TextView) findViewById(R.id.movieReleaseDate);
+        release.setText("Movie Release Date : " + mDetail.getMovieReleaseDate());
+
+        TextView runtime = (TextView) findViewById(R.id.movieRuntime);
+        runtime.setText("Movie Runtime : " + mDetail.getMovieRunTime());
     }
 }
