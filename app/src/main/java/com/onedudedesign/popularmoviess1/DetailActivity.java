5ea933efbe979837ce4d5d168043aa257f04a091
clucier <onedudedesign@gmail.com>
2017-04-08 10:53:41 -0700

    fixed and commented detail activity and the string resources, fixed scrolling issue when rotated

--- app/src/main/java/com/onedudedesign/popularmoviess1/DetailActivity.java
+++ app/src/main/java/com/onedudedesign/popularmoviess1/DetailActivity.java
@@ -10,8 +10,6 @@ import com.onedudedesign.popularmoviess1.Models.MovieDetail;
 import com.onedudedesign.popularmoviess1.utils.MovieApiService;
 import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
-
 import retrofit.Callback;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
@@ -20,7 +18,6 @@ import retrofit.client.Response;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private static final String TMDB_API_KEY = "fd66dfefac539c5f745200aadb175e4d";
     private MovieDetail mDetail = new MovieDetail();
     private String movieID;
 
@@ -29,23 +26,30 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        //receiving the Movie ID from the clicked item from MainActivity
         Intent intent = this.getIntent();
         movieID = intent.getStringExtra("movieID");
 
+        final String api_key = getResources().getString(R.string.TMDB_API_KEY);
+
+        /* we get the movie ID and make the call directly to the movie information in the API
+        because we will need additional information not returned in the Popular and Toprated
+        queries */
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint("http://api.themoviedb.org/3/")
                 .setRequestInterceptor(new RequestInterceptor() {
                     @Override
                     public void intercept(RequestFacade request) {
                         request.addPathParam("movie_id", movieID);
-                        request.addEncodedQueryParam("api_key", TMDB_API_KEY);
+                        request.addEncodedQueryParam("api_key", api_key);
                     }
                 })
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .build();
 
         MovieApiService service = restAdapter.create(MovieApiService.class);
-
+        //callback to the MovieDetail model, using a single API Service with the get requests
+        //will look to combine the adapter and service calls into a class
         service.getMovie(new Callback<MovieDetail>() {
             @Override
             public void success(MovieDetail movieDetail, Response response) {
@@ -60,6 +64,7 @@ public class DetailActivity extends AppCompatActivity {
         });
     }
 
+    //populate the detail activity screen
     private void populateData () {
 
         TextView title = (TextView) findViewById(R.id.detailMovieTitle);
@@ -71,21 +76,20 @@ public class DetailActivity extends AppCompatActivity {
         year.setText(yearString);
 
         TextView runtime = (TextView) findViewById(R.id.detailRunTime);
-        runtime.setText(mDetail.getMovieRunTime() + " Minutes");
+        runtime.setText(mDetail.getMovieRunTime() + getString(R.string.detail_act_minute_format));
 
         TextView rating = (TextView) findViewById(R.id.detailRating);
-        rating.setText(mDetail.getMovieTmdbRating() + "/10");
+        rating.setText(mDetail.getMovieTmdbRating() + getString(R.string.detail_act_rating_format));
 
         TextView overview = (TextView) findViewById(R.id.detailSynopsis);
         overview.setText(mDetail.getMovieSynopsis());
 
         ImageView poster = (ImageView) findViewById(R.id.detailPosterImage);
-
+        //using Picasso to deal with image retrieval and caching in background thread
         Picasso.with(this)
                 .load("http://image.tmdb.org/t/p/w500/" + mDetail.getMoviePoster())
-                .placeholder(R.drawable.placeholder) //this is optional the image to display while the url image is downloading
-                .error(R.drawable.error)         //this is also optional if some error has occurred in downloading the image this image would be displayed
+                .placeholder(R.drawable.placeholder) //displays temop image while loading
+                .error(R.drawable.error) //displays an error image if the load fails
                 .into(poster);
      }
 }
-//todo fix scrolling as overview off some screens and landscape
